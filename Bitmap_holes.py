1c03f46888c877d5ad8183a85f3ee40eeaa5c928
SreeHari <86615584+SREEHARI-98@users.noreply.github.com>
2022-08-01 21:58:58 +0530

    Create Bitmap_holes.py

---
+++ Bitmap_holes.py
@@ -0,0 +1,63 @@
+'''
+Have the function BitmapHoles (strarr) take the
+array of strings stored in strArr, which will be a 2D
+matrix of 0 and 1's, and determine how many holes,
+or contiguous regions of O's, exist in the matrix. A
+contiguous region is one where there is a connected
+group of O's going in one or more of four directions:
+up, down, left, or right. For example: if strArris
+["10111", "10101","11101","11111"], then this looks
+like the following matrix:
+
+1 0 1 1 1
+1 0 1 0 1
+1 1 1 0 1
+1 1 1 1 1
+output:2
+
+
+Input:"01111", "01101", "00011", "11110"
+Output:3
+Input:"1011", "0010"
+Output:2
+'''
+
+
+
+def BitmapHoles(strArr):
+    bitmap = {}
+    for i in range(len(strArr)):
+        for j in range(len(strArr[i])):
+            bitmap[(i,j)] = int(strArr[i][j])
+    
+    print(bitmap)
+    hole_count = 0
+    hole = set()
+    checked = set()
+    flag = True
+    
+    for i in range(len(strArr)):
+        for j in range(len(strArr[i])):
+            stack = [(i,j)]
+            while stack:
+                coords = stack.pop()
+                if coords not in checked:
+                    checked.add(coords)
+                    if bitmap[coords] == 0 and coords not in hole:
+                        hole.add(coords)
+                        if flag == True:
+                            hole_count += 1
+                            flag = False
+                        if coords[0] - 1 >= 0 and (coords[0]-1,coords[1]) not in checked:
+                            stack.append((coords[0]-1,coords[1]))
+                        if coords[0] + 1 < len(strArr) and (coords[0]+1,coords[1]) not in checked:
+                            stack.append((coords[0]+1,coords[1]))
+                        if coords[1] - 1 >= 0 and (coords[0],coords[1]-1) not in checked:
+                            stack.append((coords[0],coords[1]-1))
+                        if coords[1] + 1 < len(strArr[coords[0]]) and (coords[0],coords[1]+1) not in checked:
+                            stack.append((coords[0],coords[1]+1))
+            flag = True
+            
+    return hole_count
+
+print(BitmapHoles(["01111", "01101", "00011", "11110"]))
